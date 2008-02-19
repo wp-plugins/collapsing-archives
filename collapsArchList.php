f645e586bb0c92fed2144802723f94dbc45a6ade
robfelty <robfelty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-02-19 14:54:41 +0000

    still working on turning off posts
    git-svn-id: https://plugins.svn.wordpress.org/collapsing-archives/trunk@32246 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapsArchList.php
+++ collapsArchList.php
@@ -133,7 +133,7 @@ if( $allPosts ) {
       $newYear = false;
     }
 
-    if($currentMonth != $archPost->month &&  get_option('collapsArchExpandMonths')=='yes') {
+    if($currentMonth != $archPost->month) {
       $currentMonth = $archPost->month;
       $newMonth = true;
       if($newYear == false) { #close off last month
@@ -219,6 +219,71 @@ if( $allPosts ) {
       echo "          <li class='collapsArchPost'><a href='$link' title='$title_text'>$text</a>$commcount</li>\n";
       }
     } else {
+      if( get_option('collapsArchExpandMonths')=='yes' ) {
+        $link = 'javascript:;';
+        $onclick = 'onclick="hideNestedList(event); return false"';
+        $monthCollapse = 'collapsing';
+        if( get_option('collapsArchExpandCurrentMonth')=='yes'
+            && $currentYear == date('Y')
+            && $currentMonth == date('n') ) {
+									$monthRel = 'hide';
+									$monthTitle= 'click to collapse';
+          $postStyle = '';
+									$ding = '&#9660;';
+        } else {
+									$monthRel = 'show';
+									$monthTitle= 'click to expand';
+									$ding = '&#9658;';
+        }
+      } else {
+        $link = get_month_link( $currentYear, $currentMonth );
+        $onclick = '';
+        $monthRel = '';
+        $monthTitle = '';
+        $monthCollapse = '';
+      }
+      $the_link = "<span title='$monthTitle' class='$monthCollapse $monthRel' $onclick>$ding&nbsp;</span>";
+      $the_link .="<a href='$home/$archives$currentYear/$currentMonth' title='$title_text'>";
+      $the_link .="$text</a>\n";
+
+      echo "      <li class='collapsing'>".$the_link.$monthCount;
+
+    }
+    if( get_option('collapsArchShowMonths')=='yes' && get_option('collapsArchExpandMonths')=='yes' ) {
+      echo "        <ul $postStyle id=\"collapsArchList-";
+      echo "$currentYear-$currentMonth\">\n";
+      $text = '';
+
+      if( get_option('collapsArchShowPostNumber')=='yes' ) {
+        $text .= '#'.$archPost->ID;
+      }
+
+      if( get_option('collapsArchShowPostTitle')=='yes' ) {
+        $title_text = strip_tags(htmlentities($archPost->post_title, ENT_QUOTES));
+
+        if( get_option('collapsArchPostTitleLength')> 0 && strlen( $title_text ) > get_option('collapsArchPostTitleLength') ) {
+          $title_text = substr($title_text, 0, get_option('collapsArchPostTitleLength') );
+          if( get_option('collapsArchShowPostTitleEllipsis')=='yes' ) {
+            $title_text .= ' ...';
+          }
+        }
+
+        $text .= ( $text == '' ? $title_text : ' - '.$title_text );
+      }
+
+      if( get_option('collapsArchShowPostDate')=='yes' ) {
+        $theDate = mysql2date(get_option('collapsArchPostDateFormat'), $archPost->post_date );
+        $text .= ( $text == '' ? $theDate : ', '.$theDate );
+      }
+
+      if( get_option('collapsArchShowCommentCount')=='yes' ) {
+        $commcount = ' ('.get_comments_number($archPost->ID).')';
+      }
+
+/*
+      $link = get_permalink( $archPost->ID );
+      echo "          <li class='collapsArchPost'><a href='$link' title='$title_text'>$text</a>$commcount</li>\n";
+      }
       echo "          <li class='collapsArchPost'>";
       $text = '';
 
@@ -243,6 +308,7 @@ if( $allPosts ) {
       $link = get_permalink( $archPost->ID );
       echo "<a href='$link' title='$title_text'>$text</a>$commcount";
       echo "</li>\n";
+*/
     }
   }
 } ?>
